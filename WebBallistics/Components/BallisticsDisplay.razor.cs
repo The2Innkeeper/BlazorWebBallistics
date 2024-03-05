a89e175210954a8bf205b432f6925d6452d402fd
The2Innkeeper <theinnkeeper510@gmail.com>
2024-03-05 18:20:54 -0500

    Fix framerate issues

--- WebBallistics/Components/BallisticsDisplay.razor.cs
+++ WebBallistics/Components/BallisticsDisplay.razor.cs
@@ -4,6 +4,7 @@ using System.Timers;
 using Microsoft.AspNetCore.Components;
 using WebBallistics.Models;
 using WebBallistics.Services;
+using WebBallistics.Helpers;
 
 namespace WebBallistics.Components;
 
@@ -17,16 +18,16 @@ public partial class BallisticsDisplay : IDisposable
     private string trajectoryPath;
     private Vector2 currentPosition = new(0, SVG_HEIGHT);
     private System.Timers.Timer animationTimer;
-    private DateTime animationStartTime;
     private bool isAnimating = false;
     private float simulationTime = 0f;
-    private float deltaTime = 0.02f;
+    private float deltaTimeMS = 20f;
 
 
     protected override void OnInitialized()
     {
         base.OnInitialized();
         Parameters.OnChange += RefreshTrajectory;
+        Parameters.OnChange += SetupAnimationTimer;
         RefreshTrajectory();
     }
 
@@ -66,7 +67,7 @@ public partial class BallisticsDisplay : IDisposable
     private void ResetSimulation()
     {
         simulationTime = 0f; // Reset the simulation time
-        currentPosition = CartesianToSvgCoordinates(new Vector2(0, 0));
+        currentPosition = CoordinateConverter.CartesianToSvg(new Vector2(0, 0));
         RefreshTrajectory(); // Recalculate the trajectory path
     }
 
@@ -74,25 +75,19 @@ public partial class BallisticsDisplay : IDisposable
     {
         animationTimer?.Stop();
         animationTimer?.Dispose();
-        deltaTime = 1f / Parameters.FrameRate;
-        animationTimer = new(deltaTime);
+        deltaTimeMS = 1000f / Parameters.FrameRate;
+        animationTimer = new(deltaTimeMS);
         animationTimer.Elapsed += UpdateProjectilePosition;
         animationTimer.AutoReset = true;
         animationTimer.Start();
     }
 
-    // Converts a point from Cartesian coordinates to SVG coordinates
-    private Vector2 CartesianToSvgCoordinates(Vector2 cartesianPoint)
-    {
-        return new Vector2(cartesianPoint.X, SVG_HEIGHT - cartesianPoint.Y);
-    }
-
-
     private void UpdateProjectilePosition(object source, ElapsedEventArgs e)
     {
-        simulationTime += deltaTime * Parameters.TimeScale;
+        float deltaTimeInSeconds = deltaTimeMS / 1000f;
+        simulationTime += deltaTimeInSeconds * Parameters.TimeScale;
         Vector2 newCartesianPosition = AnimationService.CalculatePositionAtTime(simulationTime, Parameters);
-        currentPosition = CartesianToSvgCoordinates(newCartesianPosition);
+        currentPosition = CoordinateConverter.CartesianToSvg(newCartesianPosition);
 
         if (newCartesianPosition.Y < 0 || newCartesianPosition.X > SVG_WIDTH)
         {
